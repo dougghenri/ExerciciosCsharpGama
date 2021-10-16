5039bf6f621d07ebda92ce668ba6a4c03eeb8fa2
Douglas Henrique <douglashas05@gmail.com>
2021-10-16 11:10:06 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciciosCsharpGama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExerciciosCsharpGama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("### Exercícios C# ###");
+
+            Console.WriteLine("(1) IMC");
+            Console.WriteLine("(2) Salário");
+            Console.WriteLine("(3) Raio");
+            Console.WriteLine()
+        }
+    }
+}
